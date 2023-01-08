af9a911a15d57a9dee06587371451c2f4558a3f6
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-01-08 15:15:34 +0700

    Update Static.cpp

--- CPP/Advance/Static.cpp
+++ CPP/Advance/Static.cpp
@@ -1,1 +1,15 @@
-hello
+Biến static
+- Là biến cục bộ trong hàm nhưng tồn tại cho đến khi chương trình kết thúc
+- Biến static chỉ được khởi tạo 1 lần, các lần sau trình biên dịch sẽ bỏ qua đoạn mã đó
+  void A()
+  {
+      static int count = 0;
+      count++;
+      cout << count << endl;
+  }
+  int main()
+  {
+      A();    // 1
+      A();    // 2
+  }
+
