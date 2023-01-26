5a7afa628ae20ab6f3ee304bcedadecb97cf7098
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-01-26 14:13:08 +0700

    Update and rename print to Print.py

---
+++ Python/Print.py
@@ -0,0 +1,25 @@
+#int
+print(1 + 1)                # 2
+print(type(1 + 1))          # <class 'int'>
+
+# float
+print(1.1 + 1.1)            # 2.2
+print(type(1.1 + 1.1))      # <class 'float'>
+
+# string
+print('1' + '1')            # 11
+print(type('1' + '1'))      # <class 'str'>
+
+# string + int
+print('1' + str(1))         # 11 
+print(type('1' + str(1)))   # <class 'str'>
+
+# int + string
+print(1 + int('1'))         # 2
+print(type(1 + int('1')))   # <class 'int'>
+
+# calculate sum
+x = 3
+y = 10
+print('x + y =', x + y)
+
