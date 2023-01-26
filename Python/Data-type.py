5aac45445b585f7e6adc10ec45a2ceb6804d8a39
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-01-26 16:44:44 +0700

    Update Data-type.py

--- Python/Data-type.py
+++ Python/Data-type.py
@@ -2,12 +2,22 @@
 x = 10
 print(x)
 
+
 # float
 x = 10.0
 print(x)
 
+
 # string
 x = '10'
 print(x)
+
+
+# Biến trong python có thể thay đổi kiểu dữ liệu một cách tự động
+x = 100
+x = 'Hello'
+print(x)    # Hello
+
+
 
 
