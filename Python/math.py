35f7c4fbc684b0e3389d07dfd0f85abaa1f0dfa6
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-01-26 15:30:32 +0700

    Create math.py

---
+++ Python/math.py
@@ -0,0 +1,33 @@
+# Without import math library
+print(3 + 4)        # 7
+print(3 - 4)        # -1
+print(3 * 4)        # 12
+print(3 / 4)        # 0.75
+
+print((-1) // 2)    # -1    phép chia lấy nguyên, làm tròn hướng về âm vô cùng
+print(3 // 4)       # 0
+
+print(3 % 4)        # 3
+print(abs(-1))      # 1
+print(pow(2, 3))    # 8
+
+
+# when import math
+import math
+print(math.pi)                              # 3.141592653589793
+
+print(math.sin(math.pi / 2))                # 1.0
+print(math.cos(math.pi))                    # -1.0
+print(math.tan(math.pi / 4))                # 0.9999999999999999
+
+print(math.sqrt(4))                         # 2.0
+
+# Làm tròn
+print(math.ceil(3.9))                       # 4
+print(math.ceil(3.1))                       # 4
+print(math.ceil(math.tan(math.pi / 4)))     # 1
+
+print(math.floor(3.9))                      # 3
+print(math.floor(3.1))                      # 3
+
+
