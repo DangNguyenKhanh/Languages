b07502f74f452dab1e88bc04789f307b812163e4
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-01-26 23:57:49 +0700

    Update loop.py

--- Python/loop.py
+++ Python/loop.py
@@ -10,4 +10,22 @@ while i < 10:
 print(i)        
  
     
-#    
+# Cấu trúc while-else thực thi khi kết thúc vòng lặp bằng điều kiện while
+i = 0
+while i < 3:
+    i += 1
+else:
+    print('hello')  # hello
+
+    
+# Cấu trúc while-else sẽ không thực thi else nếu thực hiện break
+i = 0
+while True:
+    i += 1
+    if i >= 3:
+        break
+else:
+    print('hello')  # Không in ra gì hết
+
+
+ 
