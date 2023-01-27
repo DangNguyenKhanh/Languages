7c9bd62c9d2ae335b422fb6e036e744f23a72275
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-01-27 18:10:22 +0700

    Create Số_lượng_nhỏ.py

---
+++ Python/Sorting/Số_lượng_nhỏ.py
@@ -0,0 +1,12 @@
+# Interchange Sort - Đổi chỗ trực tiếp
+def interchange_sort(a):
+    n = len(a)
+    for i in range(n - 1):
+        for j in range(i+1, n):
+            if a[i] > a[j]:
+                temp = a[i]
+                a[i] = a[j]
+                a[j] = temp
+    return a
+# Trường hợp tốt nhất: O(1)
+# Trường hợp xấu nhất: O(n * (n - 1) / 2) = O(n^2)
