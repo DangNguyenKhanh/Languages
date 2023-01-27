806060d8632b6fd99de5b466ae9364a8b19a598a
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-01-27 17:44:24 +0700

    Update Search.py

--- Python/Algorithm/Search.py
+++ Python/Algorithm/Search.py
@@ -4,8 +4,29 @@ def sequential_search(a, x):
         if a[i] == x:
             return i      # if found return index
     return -1             # not found return -1
-  
-  
-# Binary search
 
+a = [1, 2, 5, 7, 8, 9]
+res = sequential_search(a, 9)
+print(res)
+  
+    
+# Binary search
+def binary_search(a, x):
+    index = -1
+    i = 0
+    j = len(a) - 1
+    while i <= j:
+        k = int((i + j) / 2)
+        if x < a[k]:
+            j = k - 1
+        elif x == a[k]:
+            index = k
+            break
+        else:
+            i = k + 1
+    return index
+
+a = [1, 2, 5, 7, 8, 9]
+res = binary_search(a, 9)
+print(res)
   
