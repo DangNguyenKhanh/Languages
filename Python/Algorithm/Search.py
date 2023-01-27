5fa45fa95615682acbb032ddf1206021e9426509
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-01-27 17:23:37 +0700

    Create Search.py

---
+++ Python/Algorithm/Search.py
@@ -0,0 +1,11 @@
+# Sequential search
+def sequential_search(a, x):
+    for i in range(len(a)):
+        if a[i] == x:
+            return i      # if found return index
+    return -1             # not found return -1
+  
+  
+# Binary search
+
+  
