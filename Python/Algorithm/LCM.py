db64e1256ae0e950400dd9b5a08167aede3e2bee
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-01-27 20:28:54 +0700

    Update LCM.py

--- Python/Algorithm/LCM.py
+++ Python/Algorithm/LCM.py
@@ -1,1 +1,18 @@
 # LCM - Least Common Multiple: Bội chung nhỏ nhất
+def GCD(a, b):
+    if a < b:
+        return GCD(b, a)
+    while b != 0:
+        r = a % b
+        a = b
+        b = r
+    return a
+
+# a * b = GCD * LCM
+def LCM(a, b):
+    res = (a * b) / GCD(a, b)
+    return res
+
+
+res = LCM(12, 4)
+print(res)
