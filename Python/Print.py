6675706120b1b90f7d041234e547d07fc21beae8
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-02-06 23:14:34 +0700

    Update Print.py

--- Python/Print.py
+++ Python/Print.py
@@ -18,14 +18,17 @@ print(type('1' + str(1)))   # <class 'str'>
 print(1 + int('1'))         # 2
 print(type(1 + int('1')))   # <class 'int'>
 
+
 # calculate sum
 x = 3
 y = 10
 print('x + y =', x + y)
 
+
 # Special quotes
 print('This is double quotes ""')   # This is double quotes ""
 print("This is single quotes ''")   # This is single quotes ''
+
 
 # Escape sequence
 print('\\')     # \
@@ -34,6 +37,12 @@ print('\"')     # "
 print('\n')     # breakline
 print('\t')     # tab
 
+
 # in cùng 1 dòng
 for i in range(5):
     print(i, end=" ")   # 0 1 2 3 4
+
+
+# In nhiều giá trị 1 lúc
+print(1, 2, "Đặng Nguyên Khánh")
+
