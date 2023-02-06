05bfa16c32c2b7ef668a84f617b16420770b26db
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-02-06 23:36:55 +0700

    Create assign-var.py

---
+++ Python/assign-var.py
@@ -0,0 +1,35 @@
+# Gán nhiều biến cùng giá trị
+x = y = z = 2
+
+
+# Gán nhiều biến khác giá trị
+x, y, z = 1, 2, 3
+
+# Sinh ra lỗi 
+x = 1, y = 2, z = 3   # ERROR: can't assign to literal
+
+
+# Hoán đổi giá trị x và y cho nhau
+x, y = 10, 3
+temp = x
+x = y
+y = temp
+print(x, y)     # 3 10
+
+# Hoán đổi cách 2
+x, y = 10, 3
+x, y = y, x
+print(x, y)     # 3 10
+
+
+# Gán thay đổi
+x = 10
+x += 1    # x = x + 1
+print(x)  # 11 
+
+x = 10
+x -= 1    # x = x - 1
+print(x)  # 9
+
+
+
