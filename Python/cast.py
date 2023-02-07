c5a9e63c72172a02252c3bd6adb1ffdb43fe70e9
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-02-07 22:41:42 +0700

    Create cast.py

---
+++ Python/cast.py
@@ -0,0 +1,18 @@
+# Ép kiểu float sang int
+x = int(2.5)  # x = 2.5
+
+# Ép kiểu str sang int
+x = int("2")      # x = 2
+x = int("a2")     # ERROR
+x = int("2a")     # ERROR
+x = int("2.5")    # ERROR
+x = int("2 + 5")  # ERROR
+
+# Ép kiểu int sang float
+x = float(2)      # 2.0
+
+# Ép kiểu str sang float
+x = float("1.2")  # 1.2
+
+
+
