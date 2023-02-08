1ccd99306fab8eb89f957b6374c137cc68a83ab1
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-02-08 14:51:19 +0700

    Create OOP.py

---
+++ Python/OOP.py
@@ -0,0 +1,20 @@
+# class SinhVien có 2 thuộc tính name, id và 2 phương thức set, get
+class SinhVien:
+    name = "NULL"
+    id = -1
+
+    def get(self):
+        print(self.name)
+        print(self.id)
+
+    def set(self, name, id):
+        self.name = name
+        self.id = id
+
+
+obj1 = SinhVien()
+obj1.get()
+
+obj2 = SinhVien()
+obj2.set("Khánh", 10)
+obj2.get()
