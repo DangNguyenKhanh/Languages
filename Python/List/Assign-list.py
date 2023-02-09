7d415f9292a1bd3debe5d960e495ba6519170254
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-02-09 16:24:43 +0700

    Update Assign-list.py

--- Python/List/Assign-list.py
+++ Python/List/Assign-list.py
@@ -1,1 +1,14 @@
+# Sự tham chiếu 2 danh sách
+a = [1, 2, 3]
+b = a
+b[0] = 10
+print(a)    # [10, 2, 3]
+
+
+# Khắc phục sự tham chiếu
+a = [1, 2, 3]
+b = a.copy()
+b[0] = 10
+print(a)    # [1, 2, 3]
+print(b)    # [10, 2, 3]
 
