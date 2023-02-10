82d604d478b6daf60c82477b9dc4103315c2e829
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-02-10 09:06:23 +0700

    Update Interchange-sort.py

--- Python/Sorting/Interchange-sort.py
+++ Python/Sorting/Interchange-sort.py
@@ -10,3 +10,14 @@ def interchange_sort(a):
     return a
 # Trường hợp xấu nhất: O(n * (n - 1) / 2) = O(n^2)
 # Không gian hỗ trợ: O(1)
+
+
+# UPDATE
+def interchange_sort(a):
+    for i in range(len(a) - 1):
+        for j in range(i + 1, len(a)):
+            if a[i] > a[j]:
+                a[i], a[j] = a[j], a[i]
+             
+            
+#
