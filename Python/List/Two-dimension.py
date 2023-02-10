eb5e8650dde14d999128a37ce8dec8e4999a7f32
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-02-10 09:22:16 +0700

    Update Two-dimension.py

--- Python/List/Two-dimension.py
+++ Python/List/Two-dimension.py
@@ -1,1 +1,15 @@
+# Khởi tạo mảng 2 chiều và in mảng 2 chiều
+array2D = []
+m = int(input())    # row
+n = int(input())    # col
+for i in range(m):
+    b = []
+    for j in range(n):
+        x = int(input("a[" + str(j) + "] = "))
+        b += [x]
+    array2D.append(b)
 
+for i in range(m):
+    for j in range(n):
+        print(array2D[i][j], sep='', end=' ')
+    print()
