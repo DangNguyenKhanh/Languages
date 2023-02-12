1bb94cd4b330c7d1277500a4304baea57067aef6
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-02-12 11:59:01 +0700

    Update String.py

--- Python/String/String.py
+++ Python/String/String.py
@@ -6,9 +6,49 @@ s = 'hello'
 s = "hello"
 
 
-# Xấu kí tự không cho phép thay đổi từng kí tự
+# Chuỗi không cho phép thay đổi từng kí tự
 s = "abc"
 s[0] = 'k'    # ERROR
 print(s)  
+
+
+# Truy xuất phần tử trong chuỗi
+s = "abc"
+print(s[0])     # a
+
+
+# Kiểm tra kiểu dữ liệu
+s = "abc"
+print(type(s))  # <class 'str'>
+
+
+# Độ dài chuỗi
+s = "abc"
+print(len(s))   # 3
+
+
+# Thực hiện nhân số nguyên cho chuỗi
+s = "abc"
+print(s*3)   # abcabcabc
+
+
+# Phép cộng giữa 2 chuỗi
+s = "abc"
+print(s+s)   # abcabc
+
+
+# Ép kiểu chuỗi
+a = 1
+b = 1.2
+c = [3, 4]
+s = str(a) + str(b) + str(c)
+print(s)        # 11.2[3, 4]
+
+
+# Xem các phương thức của class str
+print(dir(str))
+
+
+# 
 
 
