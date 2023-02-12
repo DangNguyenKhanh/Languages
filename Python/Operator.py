9bae1aad0c8b0dfd42bb5e4465d6caf7cee924ec
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-02-13 00:19:07 +0700

    Create Operator.py

---
+++ Python/Operator.py
@@ -0,0 +1,36 @@
+# Phép so sánh
+x == y
+x != y
+x > y
+x >= y
+x < y
+x <= y
+
+
+# So sánh bắc cầu
+a = 1
+print(0 < a <= 1)   # True
+
+a = 1
+b = 2
+print(1 <= a < 2 >= b)   # True
+
+
+# Phép AND
+T and T = T
+T and F = F
+F and T = F
+F and F = F
+
+
+# Phép OR
+T or T = T
+T or F = T
+F or T = T
+F or F = F
+
+
+# Phép NOT
+!T = F
+!F = T
+
