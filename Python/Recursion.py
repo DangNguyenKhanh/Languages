dbf06c7e6ccb13f3eb201db54e68f3be8c869b6d
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-02-13 01:02:01 +0700

    Update Recursion.py

--- Python/Recursion.py
+++ Python/Recursion.py
@@ -33,5 +33,25 @@ print(factorial(3)) # 6
 print(factorial(4)) # 24
 
 
-# hàm đệ quy đảo ngược reverse_list, reverse_str
+# Hàm đệ quy đảo ngược reverse_str
+def reverse_str(s):
+    if s == "":
+        return ""
+    else:
+        return s[-1] + reverse_str(s[:-1])  # s[:-1] = s[-len():-1] bắt đầu từ -len() đến -2
+
+print(reverse_str("abcde")) # edcba
+print("abcde"[ : : -1]) # edcba - Kỹ thuật slicing
+
+
+# Hàm đệ quy đảo ngược reverse_list
+def reverse_list(A):
+    if A == []:
+        return []
+    else:
+        return A[len(A)-1 : ] + reverse_list(A[ : len(A)-1])
+
+print(reverse_list([1, 2, 3, 4, 5]))
+print([1, 2, 3, 4, 5][ : : -1]) # [5, 4, 3, 2, 1]   - Kỹ thuật slicing
+
 
