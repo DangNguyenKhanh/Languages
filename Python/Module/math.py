0bf459847ae1bf8c975add3f638217fcc81198e4
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-02-12 10:30:42 +0700

    Update math.py

--- Python/Module/math.py
+++ Python/Module/math.py
@@ -53,20 +53,5 @@ print(m.pi) # 3.141592653589793
 print(m.e)  # 2.718281828459045
 
 
-# Số phức trong thư viện math
-z = 3 + 2j
-print(z)        # (3+2j)
-print(type(z))  # <class 'complex'>
-print(abs(z))   # 3.605551275463989
-
-
-# Số phức liên hợp - thêm dấu trừ phần ảo
-z = 3 + 2j
-z = z.conjugate()
-print(z)        # (3-2j)
-print(type(z))  # <class 'complex'>
-print(abs(z))   # 3.605551275463989
-print(z.real)   # 3.0
-print(z.imag)   # -2.0
 
 
