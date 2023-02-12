07065e8363dd0af2e01e3040a8acfa8635f34ca8
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-02-12 15:42:52 +0700

    Update Two-dimension.py

--- Python/List/Two-dimension.py
+++ Python/List/Two-dimension.py
@@ -13,3 +13,19 @@ for i in range(m):
     for j in range(n):
         print(array2D[i][j], sep='', end=' ')
     print()
+
+        
+# Random matrix n x n
+from random import random
+n = 3
+array2D = []
+for i in range(n):
+    x = []
+    for j in range(n):
+        x.append(int(100 * random()) + 1)
+    array2D.append(x)
+
+for i in range(n):
+    for j in range(n):
+        print(array2D[i][j], end=' ')
+    print() 
