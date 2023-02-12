a156a89713e275c03f2542d055dbe9ccb1d2f386
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-02-12 11:51:40 +0700

    Create String.py

---
+++ Python/String/String.py
@@ -0,0 +1,14 @@
+# Khai báo bằng ngoặc đơn
+s = 'hello'
+
+
+# Khai báo bằng ngoặc kép
+s = "hello"
+
+
+# Xấu kí tự không cho phép thay đổi từng kí tự
+s = "abc"
+s[0] = 'k'    # ERROR
+print(s)  
+
+
