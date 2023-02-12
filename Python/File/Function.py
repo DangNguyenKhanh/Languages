bf5bf5d49cd374e15183f8411145a6872ebdb17b
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-02-12 23:14:54 +0700

    Update Function.py

--- Python/File/Function.py
+++ Python/File/Function.py
@@ -12,4 +12,41 @@ continue_content = file.read()
 print(continue_content)   # '' rỗng vì đầu đọc đã ở cuối file
 
 
-#  
+# Hàm f.tell() cho biết vị trí đọc cũng chính là số lượng byte đã đọc được
+f = open("inp.dat", 'r')
+print(f.tell())     # 0
+content = f.read()
+print(content)
+print(f.tell())     # 45 số lượng byte đã đọc được
+
+
+# Đóng tệp đang mở
+f = open("inp.dat", 'r')
+content = f.read()
+f.close()   # Khi đóng tệp không còn dùng được tell() nữa
+
+
+# Hàm f.readline() đọc dữ liệu 1 dòng
+f = open("inp.dat", 'r')
+print(f.tell())                 # 0
+print(f.readline(), end='')     # hello world
+print(f.tell())                 # 13
+print(f.readline(), end='')     # 1 2 3 4 5
+print(f.tell())                 # 24
+print(f.readline(), end='')     # 1.1 2.2 3.3 4.4 5.5
+print(f.tell())                 # 45
+f.close()
+
+# Đưa 1 2 3 4 5 vào list
+f = open("inp.dat", 'r')
+print(f.readline(), end='')     # hello world
+s = f.readline()
+a = s.split()
+print(a)        # ['1', '2', '3', '4', '5']
+for i in range(len(a)):
+    a[i] = int(a[i])
+print(a)        # [1, 2, 3, 4, 5]
+f.close()
+
+
+
