87ccc42b4eed16573b39c58292505eae664b815e
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-02-14 00:29:27 +0700

    Update Basic.py

--- Python/Graphics/Basic.py
+++ Python/Graphics/Basic.py
@@ -13,3 +13,14 @@ t.pendown()
 t.home()
 
 
+# Thiết lập cửa sổ 
+t.setup(480, 360, startx=100, starty=200)
+- Ngang 480 dọc 360 cách bên trái màn hình desktop 100 và cách bên trên 200
+
+
+# Kiểm tra độ rộng canvas
+t.setup(480, 360, startx=100, starty=200)
+print(t.screensize())   # canvas = (400, 300)
+
+
+# 
