8a4dca294c2338d40d244633a59d4a7b78fb456e
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-02-13 23:27:50 +0700

    Create Begin.py

---
+++ Python/Graphics/Begin.py
@@ -0,0 +1,37 @@
+# Tạo ra con rùa ở giữa màn hình (tọa độ 0,0) đang hướng về phải và đi 100 bước
+import turtle as t
+import time
+
+t.fd(100)       # Tiến về phía trước 100 bước = 100 pixel
+t.shape("turtle")
+time.sleep(5)   # ngăn đóng window
+
+
+# Kích thước canvas - khung vẽ
+- Kích thước 640 x 480
+         90 độ
+180 độ            0 độ
+        -90 độ
+
+  
+# Cho biết hướng - heading()
+print(t.heading())  # 0 độ
+
+
+
+# Xem tọa độ (x, y)
+print(t.xcor(), t.ycor())   # 100.0 0.0
+
+
+# Nâng bút
+t.penup()
+
+
+# Hạ bút (mặc định)
+t.pendown()
+
+
+# Kiểm tra đang hạ bút 
+if t.isdown():
+  
+  
